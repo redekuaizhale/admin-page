294cdc19d8266d4be405f664eecdbd7ef1fef396
zhanghui <17343009527@163.com>
2020-06-14 21:47:19 +0800

    update

---
+++ src/components/home-card-total/home-card-total.vue
@@ -0,0 +1,46 @@
+<template>
+  <Card>
+    <p slot="title">
+      {{ title }}
+    </p>
+    <div class="content">
+      <span class="num-total">
+        25,848
+      </span>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  </Card>
+</template>
+
+<script>
+export default {
+  name: 'HomeCardTotal',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+
+  },
+  data() {
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<style lang="less">
+  .HomeCardTotal{
+    .content{
+        border-bottom: 1px solid #e8eaec;
+    }
+  }
+
+</style>
