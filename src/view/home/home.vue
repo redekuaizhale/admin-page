a6480d06ad28ef2dc0375beed12cf6e282de54c2
张辉 <zhui@dxct.org>
2020-08-26 14:58:14 +0800

    update

--- src/view/home/home.vue
+++ src/view/home/home.vue
@@ -14,36 +14,36 @@
       <div>
         <Row>
           <Col :xs="24" :md="16">
-          <h4>访问量趋势</h4>
-          <HomeTreatTotal class="height-250"/>
+            <h4>访问量趋势</h4>
+            <HomeTreatTotal class="height-250"/>
           </Col>
           <Col :xs="24" :md="8">
-          <h4>访问量趋势</h4>
-          <HomeSickTypeTotal class="height-250"/>
+            <h4>访问量趋势</h4>
+            <HomeSickTypeTotal class="height-250"/>
           </Col>
         </Row>
       </div>
     </Card>
     <Row :gutter="16" class="row">
       <Col :xs="24" :md="12">
-      <Card>
-        <p slot="title">
-          卡片2
-        </p>
-        <div>
-          <HomeSickTypePie/>
-        </div>
-      </Card>
+        <Card>
+          <p slot="title">
+            卡片2
+          </p>
+          <div>
+            <HomeSickTypePie/>
+          </div>
+        </Card>
       </Col>
       <Col :xs="24" :md="12">
-      <Card>
-        <p slot="title">
-          卡片3
-        </p>
-        <div>
-          <HomeDeptTotalLine/>
-        </div>
-      </Card>
+        <Card>
+          <p slot="title">
+            卡片3
+          </p>
+          <div>
+            <HomeDeptTotalLine/>
+          </div>
+        </Card>
       </Col>
     </Row>
   </div>
@@ -52,20 +52,20 @@
 <script>
 import HomeTreatTotal from '../../components/home-chart/home-treat-total'
 import HomeSickTypeTotal from '../../components/home-chart/home-sick-type-total'
-import AvatorIcon from '../../components/avator-icon/avator-icon'
 import HomeSickTypePie from '../../components/home-chart/home-sick-type-pie'
 import HomeDeptTotalLine from '../../components/home-chart/home-dept-total-line'
+
 export default {
   name: 'Home',
-  components: { HomeDeptTotalLine, HomeSickTypePie, AvatorIcon, HomeSickTypeTotal, HomeTreatTotal },
+  components: { HomeDeptTotalLine, HomeSickTypePie, HomeSickTypeTotal, HomeTreatTotal },
   created() {
   }
 }
 </script>
 
 <style lang="less">
-  .home{
-    .row{
+  .home {
+    .row {
       margin-top: 10px;
     }
   }
